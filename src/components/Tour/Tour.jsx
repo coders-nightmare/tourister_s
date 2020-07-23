08895ea0cc26c1573276c50c3ec9acbeca6c6318
Ritik <ritik09052000@gmail.com>
2020-07-23 16:58:50 +0530

    setuped city tour project

---
+++ src/components/Tour/Tour.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./tour.scss";
+
+export default class Tour extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from tour</h1>
+      </div>
+    );
+  }
+}
